ebc3f84c19082a483d505b974b4294a0384f0f28
CKrystian <krystian.chryn@gmail.com>
2017-05-03 12:09:04 +0200

    update reg

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_format_of :email, with: /\@grenoble-em\.com/, message: 'You should have an email from grenoble-em.com'      
 end
