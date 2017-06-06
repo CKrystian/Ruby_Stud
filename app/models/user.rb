e8ffdb1adeada89420531bfd0ec44f090aa81c8d
CKrystian <krystian.chryn@gmail.com>
2017-06-07 00:01:44 +0200

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  validates_format_of :email, with: /\@stud.prz.edu\.pl/, message: 'You should have an email from stud.prz.edu.pl'      
+  #validates_format_of :email, with: /\@stud.prz.edu\.pl/, message: 'You should have an email from stud.prz.edu.pl'      
 end
