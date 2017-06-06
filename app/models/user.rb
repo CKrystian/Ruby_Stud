5188169b0b1da996985d7287b9e9a1aa44191489
CKrystian <krystian.chryn@gmail.com>
2017-06-06 23:54:06 +0200

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates_format_of :email, with: /\@stud.prz.edu\.pl/, message: 'You should have an email from stud.prz.edu.pl'      
 end
